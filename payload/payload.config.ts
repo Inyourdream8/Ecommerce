50befc2120cc305ba7af9b79bb385137e3ad47f3
Inyourdream8 <gabrielivansanchez8@gmail.com>
2025-01-22 02:00:49 +0800

    Update payload.config.ts

--- payload/payload.config.ts
+++ payload/payload.config.ts
@@ -1,1 +1,13 @@
-import { payloadCloud } from '@payloadcms/plugin-cloud'
+import { withPayload } from "@payloadcms/next/withPayload";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your Next.js config here
+  experimental: {
+    reactCompiler: false,
+  },
+};
+
+// Make sure you wrap your `nextConfig`
+// with the `withPayload` plugin
+export default withPayload(nextConfig);
